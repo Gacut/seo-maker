b35ec8e59d0976118daa86c7740979d0546a3ae6
Maksym Sierszen <yellowsoysauce@gmail.com>
2025-03-17 13:20:34 +0100

    style: removed unused commented code

--- clients/claude_client.py
+++ clients/claude_client.py
@@ -34,7 +34,6 @@ class ClaudeClient:
         
         self.MAX_RETRIES = 3
         self.DELAY = 5
-      
         
     
     
@@ -86,36 +85,3 @@ class ClaudeClient:
                     print(f"Przekroczono ilość prób. Spróbuj ponownie później. Error: {e}")
                     config["output_window"].insert("end", f"Przekroczono ilość prób. Spróbuj ponownie później. Error: {e}\n\nSprawdź status dostępności API na https://status.anthropic.com/")
                     
-    # def createMessage(self):
-    #     inputText = self.gui.paraphraser.extractContent(self.gui.HTMLWindowTab3)
-        
-    #     for attempt in range(self.MAX_RETRIES):
-    #         try:
-    #             response = self.client.messages.create(
-    #                 model="claude-3-5-sonnet-20241022",
-    #                 max_tokens=1000,
-    #                 temperature=0,
-    #                 system= self.prompt,
-    #                 messages=[
-    #                     {
-    #                         "role": "user",
-    #                         "content": [
-    #                             {
-    #                                 "type": "text",
-    #                                 "text": inputText
-    #                             }
-    #                         ]
-    #                     }
-    #                 ]
-    #             )
-    #             message = response.content[0].text
-    #             self.gui.textWindowTab3.delete("1.0", tk.END)
-    #             self.gui.textWindowTab3.insert("end", message)
-    #             return
-    #         except Exception as e:
-    #             if attempt < self.MAX_RETRIES -1:
-    #                 print(f"Połączenie {attempt + 1} zawiodło. Ponowna próba za {self.DELAY} sekund...")
-    #                 time.sleep(self.DELAY)
-    #             else:
-    #                 print(f"Przekroczono ilość prób. Spróbuj ponownie później. Error: {e}")
-    #                 self.gui.textWindowTab3.insert("end", f"Przekroczono ilość prób. Spróbuj ponownie później. Error: {e}\n\nSprawdź status dostępności API na https://status.anthropic.com/")
